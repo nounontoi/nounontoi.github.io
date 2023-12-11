4f4ac0e2a617bed349f41bf10b9acde4a934eccd
nounontoi <alexander.j.klar@gmail.com>
2023-12-11 17:25:59 +1100

    Updates

--- scripts.js
+++ scripts.js
@@ -1,3 +1,23 @@
-setInterval(function(updateClock) {
-    document.getElementById('time').innerHTML = Date()
-}, 10);
+// CLOCK
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    // let s = today.getSeconds();
+    m = checkTime(m);
+    // s = checkTime(s);
+    document.getElementById('time').innerHTML =  h + ":" + m;
+    setTimeout(startTime, 1000);
+  }
+  
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
+
+var element = document.documentElement;
+function openFullscreen() {
+    if (element.requestFullscreen) {
+        element.requestFullscreen()
+    }
+}
