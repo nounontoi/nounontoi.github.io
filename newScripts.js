ee494f4d09f9b3e2b087e8042f4f3676e54358c8
nounontoi <alexander.j.klar@gmail.com>
2024-06-29 12:35:56 +1000

    Well... I'm just gonna say that there's a lot of work to be done if I actually want it to look good. More logic, UI changes, and in the process of making a home page.

--- newScripts.js
+++ newScripts.js
@@ -1,13 +1,15 @@
+// Functions
 function randomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
 function RollDice(sides) {
     rollCount = document.getElementById("rollCount").value;
+    modifier = document.getElementById("modifier").value;
     const rollResults = [];
     for (i = 0; i < rollCount; i++) {
         roll = randomInt(1, sides + 1)
-        rollResults[i] = roll;
+        rollResults[i] = roll + Number(modifier);
     }
     const sum = rollResults.reduce((partialSum, a) => partialSum + a, 0);
     AddHistory(sides, rollResults, sum)
@@ -29,6 +31,11 @@ function IncrementRollCount(value) {
     document.getElementById("rollCount").value = Number(a) + value;
 }
 
+function IncrementModifier(value) {
+    a = document.getElementById("modifier").value;
+    document.getElementById("modifier").value = Number(a) + value;
+}
+
 function IncrementInitiative(value) {
     a = document.getElementById("initiativeModifier").value;
     document.getElementById("initiativeModifier").value = Number(a) + value;
@@ -36,10 +43,21 @@ function IncrementInitiative(value) {
 
 function AddHistory(sides, rolls, sum) {
     previousRolls = document.getElementById("rollHistory").innerHTML;
-    document.getElementById("rollHistory").innerHTML = "<span style='color: #00cccc'>d" + sides + "</span><br>" + rolls.join(", ") + "<br>Sum: " + sum + "<br><br><br>" + previousRolls;
+    document.getElementById("rollHistory").innerHTML = "<div class='historyElements'><span style='color: #00cccc'>d" + sides + "</span><br>" + rolls.join(", ") + "<br>Sum: " + sum + "</div>" + previousRolls;
 }
 
 function AddInitHis(roll, modifier, total) {
     previousRolls = document.getElementById("rollHistory").innerHTML;
-    document.getElementById("rollHistory").innerHTML = "<span style='color: #00cc00'>Initiative</span><br>" + roll + " + " + modifier + "<br>Total: " + total + "<br><br><br>" + previousRolls;
+    document.getElementById("rollHistory").innerHTML = "<div class='historyElements'><span style='color: #00cc00'>Initiative</span><br>" + roll + " + " + modifier + "<br>Total: " + total + "</div>" + previousRolls;
+}
+
+document.getElementById("rollCount").addEventListener("blur", ValidateCount);
+function ValidateCount() {
+    a = document.getElementById("rollCount").value;
+    if (a < 1) {
+        document.getElementById("rollCount").value = 1;
+    }
+    else if (a > 99) {
+        document.getElementById("rollCount").value = 99;
+    }
 }
