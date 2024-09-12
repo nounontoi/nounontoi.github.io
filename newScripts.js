b033836eb878fe8c172f37a84b4574b00755b29b
nounontoi <alexander.j.klar@gmail.com>
2024-09-12 21:41:12 +1000

    Major update to UI. More pages. Switched to flexbox for consistency and easier maintainability.

--- newScripts.js
+++ newScripts.js
@@ -1,5 +1,44 @@
+// On load
+window.addEventListener("DOMContentLoaded", function () {
+    // this.window.sessionStorage.setItem("currentTheme", "blue");
+    switch (window.location.pathname) {
+        case "/RPGTools.html":
+            LoadHistory();
+            break;
+        case "/index.html":
+            var title = this.document.getElementById("title");
+            title.style.transition = "all 0.4s ease-out";
+            title.style.fontSize = "0";
+            setTimeout(function () {
+                title.style.fontSize = "7vw";
+            });
+            break;
+    }
+});
+
 // Functions
-function randomInt(min, max) {
+function ChangeTheme() {
+    var root = document.querySelector(":root");
+    var rootS = getComputedStyle(root);
+    switch (rootS.getPropertyValue("--currentTheme")) {
+        case '"orange"':
+            root.style.setProperty("--currentTheme", '"blue"');
+            root.style.setProperty("--menuColour", "#2E236C");
+            root.style.setProperty("--buttonColour", "#2E236C;");
+            root.style.setProperty("--buttonColourHover", "#3c2e8b");
+            root.style.setProperty("--borderColour", "#433D8B");
+            break;
+        case '"blue"':
+            root.style.setProperty("--currentTheme", '"orange"');
+            root.style.setProperty("--menuColour", "#ff8906");
+            root.style.setProperty("--buttonColour", "#ff8906");
+            root.style.setProperty("--buttonColourHover", "#ff9b28");
+            root.style.setProperty("--borderColour", "#e53170");
+            break;
+    }
+}
+
+function RandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
@@ -8,22 +47,20 @@ function RollDice(sides) {
     modifier = document.getElementById("modifier").value;
     const rollResults = [];
     for (i = 0; i < rollCount; i++) {
-        roll = randomInt(1, sides + 1)
+        roll = RandomInt(1, sides + 1)
         rollResults[i] = roll + Number(modifier);
     }
     const sum = rollResults.reduce((partialSum, a) => partialSum + a, 0);
-    AddHistory(sides, rollResults, sum)
-    document.getElementById("rollResults").innerHTML = "Rolled: " + rollCount + "d" + sides + "<br><br>" + rollResults.join("<br>") + "<br><br>" + "Sum: " + sum;
-    return rollResults;
+    UpdateHistory(("d" + sides), rollResults, sum);
+    document.getElementById("rollResults").innerHTML = "<span style='font-size: 40px'>" + rollCount + "d" + sides + "</span><br><br>" + rollResults.join("<br>") + "<br><br>" + "Sum: " + sum;
 }
 
 function RollInitiative() {
-    roll = randomInt(1, 21)
+    roll = RandomInt(1, 21)
     modifier = document.getElementById("initiativeModifier").value;
     total = Number(roll) + Number(modifier);
-    AddInitHis(roll, modifier, total)
-    document.getElementById("rollResults").innerHTML = "Rolled initiative" + "<br><br>" + "Modifier: " + modifier + "<br><br>" + roll + " + " + modifier + "<br><br>" + total + "<br><br>";
-    return rollResults;
+    document.getElementById("rollResults").innerHTML = "<span style='font-size: 40px'>Initiative" + "</span><br><br>" + "Modifier: " + modifier + "<br><br>" + roll + " + " + modifier + "<br><br>" + total + "<br><br>";
+    UpdateHistory("Initiative", [roll], [total]);
 }
 
 function IncrementRollCount(value) {
@@ -41,14 +78,23 @@ function IncrementInitiative(value) {
     document.getElementById("initiativeModifier").value = Number(a) + value;
 }
 
-function AddHistory(sides, rolls, sum) {
+function UpdateHistory(rollType, rolls, sum) {
     previousRolls = document.getElementById("rollHistory").innerHTML;
-    document.getElementById("rollHistory").innerHTML = "<div class='historyElements'><span style='color: #00cccc'>d" + sides + "</span><br>" + rolls.join(", ") + "<br>Sum: " + sum + "</div>" + previousRolls;
+    document.getElementById("rollHistory").innerHTML = "<div class='historyElements'><span style='color: #e53170'>" + rollType + "</span><br>" + rolls.join(", ") + "<br>Sum: " + sum + "</div>" + previousRolls;
+    previousRolls = document.getElementById("rollHistory").innerHTML;
+    window.localStorage.setItem(rollHistory, previousRolls);
 }
 
-function AddInitHis(roll, modifier, total) {
-    previousRolls = document.getElementById("rollHistory").innerHTML;
-    document.getElementById("rollHistory").innerHTML = "<div class='historyElements'><span style='color: #00cc00'>Initiative</span><br>" + roll + " + " + modifier + "<br>Total: " + total + "</div>" + previousRolls;
+function LoadHistory() {
+    previousRolls = window.localStorage.getItem(rollHistory);
+    if (previousRolls) {
+        document.getElementById("rollHistory").innerHTML = previousRolls;
+    }
+}
+
+function ClearHistory() {
+    document.getElementById("rollHistory").innerHTML = "";
+    window.localStorage.setItem(rollHistory, "");
 }
 
 document.getElementById("rollCount").addEventListener("blur", ValidateCount);
@@ -61,3 +107,14 @@ function ValidateCount() {
         document.getElementById("rollCount").value = 99;
     }
 }
+
+function ToggleImage() {
+    if (document.getElementById("imageToggle").checked) {
+        document.getElementsByClassName("backgroundImage")[0].style.display = "block";
+    }
+    else {
+        document.getElementsByClassName("backgroundImage")[0].style.display = "none";
+    }
+}
+
+function ScrollToTop() { scrollTo({ top: 0, behavior: 'smooth' }) }
